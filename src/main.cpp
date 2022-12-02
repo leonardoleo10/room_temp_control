79704538a69dd67d95e8131e8115a7bec378925a
Leo Monteiro <leo.glmonteiro@gmail.com>
2022-12-02 08:00:05 +0000

    Changing libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <DS3231.h>
+#include <RtcDS3234.h>
 #include <DHT.h>
 
 #define DHTPIN 2 //Pin to connect DHT sensor
 #define DHTTYPE DHT11
 
-//RTC object
-DS3231 Rtc;
 DHT Dht(DHTPIN, DHTTYPE);
 
 float temp; //Actual temperature in celsius
